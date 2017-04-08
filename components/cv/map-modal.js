8ffc992a905f85207788c0e44561cafc23343a2c
Eugene Mirotin <emirotin@gmail.com>
2017-04-09 01:10:03 +0300

    google maps

--- components/cv/map-modal.js
+++ components/cv/map-modal.js
@@ -1,5 +1,27 @@
 import { Component } from 'react'
-import { Modal, Button } from 'react-bootstrap'
+import { Modal, Button, ProgressBar } from 'react-bootstrap'
+import { GoogleMap, Marker, withGoogleMap } from 'react-google-maps'
+import withScriptjs from 'react-google-maps/lib/async/withScriptjs';
+
+import config from '../../config'
+
+const Map = withScriptjs(withGoogleMap(props => {
+	const marker = {
+		position: {
+			lat: props.lat,
+			lng: props.lng,
+		},
+		key: props.markerName,
+		defaultAnimation: 2,
+	}
+	return (
+		<GoogleMap
+			defaultZoom={props.zoom}
+			defaultCenter={{ lat: props.lat, lng: props.lng }}>
+			<Marker {...marker} />
+		</GoogleMap>
+	)
+}))
 
 export default class MapModal extends Component {
 	render() {
@@ -7,11 +29,32 @@ export default class MapModal extends Component {
 
 		return (
 			<Modal show={showModal} onHide={closeModal}>
+				<style jsx>{`
+					:global(.map) {
+						height: 400px;
+					}
+				`}</style>
 				<Modal.Header closeButton>
 					<Modal.Title>Wut? Krakozhia?</Modal.Title>
 				</Modal.Header>
-				<Modal.Body>
-					LOREM IPSUM
+				<Modal.Body className="map">
+					<Map
+						containerElement={
+							<div style={{ height: `100%` }} />
+						}
+						mapElement={
+							<div style={{ height: `100%` }} />
+						}
+						googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${config.MAPS_API_KEY}`}
+						loadingElement={
+							<div style={{ height: `100%` }}>
+								<ProgressBar active striped bsStyle="info" now={100} />
+							</div>
+						}
+						zoom={config.MAP_CONFIG.zoom}
+						lat={config.MAP_CONFIG.lat}
+						lng={config.MAP_CONFIG.lng}
+						markerName="Minsk" />
 				</Modal.Body>
 				<Modal.Footer>
 					<Button onClick={closeModal}>Close</Button>
