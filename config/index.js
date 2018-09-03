24b34028728b8f1d85655028937d072e7e4fe2cc
Eugene Mirotin <emirotin@gmail.com>
2018-09-03 17:54:37 +0500

    cleanup config

--- config/index.js
+++ config/index.js
@@ -2,11 +2,5 @@ export default {
   BIRTH_DATE: "1985-10-26",
   CAREER_START_DATE: "01-05-2004",
   PROGRAMMING_START_DATE: "01-10-2011",
-  MAPS_API_KEY: "AIzaSyBXfayCu_fu5PMwBw45A_PcTU_b3KVndFw",
-  MAP_CONFIG: {
-    lat: 53.8838884,
-    lng: 27.5949741,
-    zoom: 4
-  },
   GOOGLE_VERIFICATION_CODE: "ydP1PthqJ33poe4JASwB7LX-ADx3Zak_81ouWwUVsV8"
 };
