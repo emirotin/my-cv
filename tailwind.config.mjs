0d6c4b121f1010a43d90f41741de261098de7449
Eugene Mirotin <emirotin@gmail.com>
2024-04-03 15:03:11 +0300

    add animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,34 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        zoomAndRotate1: {
+          "0%": {
+            transform: "scale(1) rotateZ(0)",
+            opacity: "1",
+          },
+          "50%, 100%": {
+            transform: "scale(0) rotateZ(359deg)",
+            opacity: "0.25",
+          },
+        },
+        zoomAndRotate2: {
+          "0%, 50%": {
+            transform: "scale(0) rotateZ(359deg)",
+            opacity: "0.25",
+          },
+          "100%": {
+            transform: "scale(1) rotateZ(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        tail: "zoomAndRotate1 10s alternate infinite ease-in",
+        wind: "zoomAndRotate2 10s alternate infinite ease-out",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
