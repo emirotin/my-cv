f3e805acaf1f2537f06cfae73d87c924353ad6ec
Eugene Mirotin <emirotin@gmail.com>
2024-04-03 13:44:50 +0300

    style theme picker

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -27,16 +27,22 @@ const ThemeSwitcher: React.FC = () => {
   }, [theme]);
 
   return (
-    <select
-      name="themeSwitcher"
-      id="themeSwitcher"
-      value={theme ?? "auto"}
-      onChange={(e) => setTheme(e.target.value)}
-    >
-      <option value="auto">Auto</option>
-      <option value="light">Light theme</option>
-      <option value="dark">Dark theme</option>
-    </select>
+    <div className="flex gap-2 items-center">
+      <label htmlFor="themeSwitcher" className="font-bold">
+        Theme:
+      </label>
+      <select
+        name="themeSwitcher"
+        id="themeSwitcher"
+        value={theme ?? "auto"}
+        onChange={(e) => setTheme(e.target.value)}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option value="auto">Auto</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </div>
   );
 };
 
